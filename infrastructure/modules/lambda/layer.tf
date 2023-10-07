bb26675b3189b83afb64298e044f22d44c3b7465
ian-soares <ian.soaresss@gmail.com>
2023-10-07 06:27:23 -0300

    ci: removing steps

--- infrastructure/modules/lambda/layer.tf
+++ infrastructure/modules/lambda/layer.tf
@@ -1,7 +1,8 @@
 resource "aws_lambda_layer_version" "lambda_layer" {
-  layer_name = var.lambda_function_layer_name
-  s3_bucket  = var.lambda_function_layer_s3_bucket
-  s3_key     = var.lambda_function_layer_s3_key
+  layer_name       = var.lambda_function_layer_name
+  filename         = "${path.module}/chansey-layer.zip"
+  #   s3_bucket  = var.lambda_function_layer_s3_bucket
+  #   s3_key     = var.lambda_function_layer_s3_key
 
   compatible_runtimes = [var.lambda_function_runtime]
 }
