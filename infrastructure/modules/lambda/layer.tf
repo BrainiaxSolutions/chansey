30faf24d90c7aacbde6f7a22bfed2d6637bdc012
ian-soares <ian.soaresss@gmail.com>
2023-10-07 05:39:02 -0300

    fix: removing depends_on

--- infrastructure/modules/lambda/layer.tf
+++ infrastructure/modules/lambda/layer.tf
@@ -4,7 +4,6 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   s3_key     = var.lambda_function_layer_s3_key
 
   compatible_runtimes = [var.lambda_function_runtime]
-  depends_on = [ aws_s3_object.deps_layer_code_zip ]
 }
 
 data "aws_lambda_layer_version" "base_lambda_layer" {
