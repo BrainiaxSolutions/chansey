9f64ddf2d88197a2dfc5447fa58230ca4fcf7fcc
ian-soares <ian.soaresss@gmail.com>
2023-10-07 05:33:58 -0300

    fix: removing zip

--- infrastructure/modules/lambda/layer.tf
+++ infrastructure/modules/lambda/layer.tf
@@ -7,18 +7,6 @@ resource "aws_lambda_layer_version" "lambda_layer" {
   depends_on = [ aws_s3_object.deps_layer_code_zip ]
 }
 
-data "archive_file" "deps_layer_code_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../../../node_modules/"
-  output_path = "${path.module}/../../../node_modules/deps.zip"
-}
-
 data "aws_lambda_layer_version" "base_lambda_layer" {
   layer_name = var.lambda_function_base_layer_name
-}
-
-resource "aws_s3_object" "deps_layer_code_zip" {
-  bucket = var.lambda_function_layer_s3_bucket
-  key    = "chansey/chansey-layer.zip"
-  source = data.archive_file.deps_layer_code_zip.output_path
 }
