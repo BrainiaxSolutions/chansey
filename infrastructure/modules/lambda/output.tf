f75b0506f9df036a5eef8d71a6e0a14fe18a271e
ian-soares <ian.soaresss@gmail.com>
2024-03-19 22:26:11 -0300

    conf: removing api gateway output

--- infrastructure/modules/lambda/output.tf
+++ infrastructure/modules/lambda/output.tf
@@ -1,6 +1,6 @@
-output "base_url" {
-  value = aws_api_gateway_deployment.api_gateway_deployment.invoke_url
-}
+# output "base_url" {
+#   value = aws_api_gateway_deployment.api_gateway_deployment.invoke_url
+# }
 
 output "lambda_function_arn" {
   value = aws_lambda_function.lambda_function.arn
